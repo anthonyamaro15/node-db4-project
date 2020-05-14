f0e368c3e01cb9f929952853a8cfc86d79aceb81
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-14 14:36:24 -0700

    added routes for stretch

--- data/seeds/05-connection.js
+++ data/seeds/05-connection.js
@@ -2,6 +2,8 @@ exports.seed = function (knex) {
   // Inserts seed entries
   return knex("ingredient_instruction_recipe").insert([
     { instruction_id: 1, ingredient_id: 2, recipe_id: 1 },
-    { instruction_id: 2, ingredient_id: 2, recipe_id: 3 },
+    { instruction_id: 2, ingredient_id: 1, recipe_id: 1 },
+    { instruction_id: 1, ingredient_id: 2, recipe_id: 3 },
+    { instruction_id: 1, ingredient_id: 2, recipe_id: 4 },
   ]);
 };
